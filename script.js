ec4e7523a2facbd57efeb0564c89cd808148cca2
Saul <64290450+murdoc007@users.noreply.github.com>
2023-05-01 18:49:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// e - enter
+// o - ober
+// i - imes
+// a - ai
+// u - ufat
+
+function encriptar() {
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+    
+    document.getElementById("imgMco").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("imgMco").style.display = "inherit";
+}
+
+function desencriptar() {
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+    
+    document.getElementById("imgMco").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("imgMco").style.display = "inherit";
+}
+
+function copy() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+    alert("Copiado exitoso");
+}
